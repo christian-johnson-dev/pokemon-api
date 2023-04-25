07180af74a591f27b2ccd8fd0f6ff33d9f7a2d54
Christian Johnson <charcoaldust@mac.com>
2023-04-25 16:44:46 -0700

    initalized project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [pokemonData, setPokemonData] = useState([]);
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=40")
+      .then((response) => response.json())
+      .then((response) => setPokemonData(response.results));
+  }, []);
+  console.log(pokemonData);
+  return (
+    <div className="App">
+      <h1>Rollcall</h1>
+      {pokemonData.length > 0 &&
+        pokemonData.map((pokemon) => {
+          return <div key={pokemon.id}>{pokemon.name}</div>;
+        })}
+    </div>
+  );
 }
-
 export default App;
